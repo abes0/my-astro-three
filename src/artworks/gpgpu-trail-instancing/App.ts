c3cf96f955fb8d8c66ac98592f7153f71124ae2d
abes0 <kangunshinu@gmail.com>
2024-02-04 17:34:29 +0900

    clean code

--- src/artworks/gpgpu-trail-instancing/App.ts
+++ src/artworks/gpgpu-trail-instancing/App.ts
@@ -9,7 +9,6 @@ import vs from "./vertex.vert?raw"
 import GPGPUSimulation from "@three/GPGPUSimulation"
 
 const WIDTH = 100
-// const NUMBER = WIDTH * WIDTH
 
 export default class App extends TemplateArtwork {
   computeRenderer?: GPUComputationRenderer
@@ -30,12 +29,8 @@ export default class App extends TemplateArtwork {
 
   constructor() {
     super()
-    CommonWork.addAmbientLight()
-    // CommonWork.addDrirectionalLight()
     CommonWork.addOrbitControls()
-
     CommonWork?.cameraSyncScreen()
-    // CommonWork.addExampleBox()
     this.onInit()
     this.createTrails()
   }
@@ -65,79 +60,6 @@ export default class App extends TemplateArtwork {
     }
   }
 
-  // createTrails() {
-  //   const box = new THREE.BoxGeometry(2, 2, 2)
-  //   const boxVertexArray = [
-  //     1, 1, 1, 1, 1, -1, 1, -1, 1, 1, -1, -1, -1, 1, -1, -1, 1, 1, -1, -1, -1,
-  //     -1, -1, 1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, 1, 1, -1, -1, 1, 1, -1, 1,
-  //     -1, -1, -1, 1, -1, -1, -1, 1, 1, 1, 1, 1, -1, -1, 1, 1, -1, 1, 1, 1, -1,
-  //     -1, 1, -1, 1, -1, -1, -1, -1, -1,
-  //   ]
-  //   const _boxVertexArray = [
-  //     1, 1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, -1, 1, 1, -1, 1, -1, -1, -1, -1,
-  //     -1, -1, 1, -1, 1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1,
-  //     1, -1, 1, -1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, -1, 1, -1, 1,
-  //     1, -1, 1, -1, -1, -1, -1, -1,
-  //   ]
-  //   const boxIndeces = [
-  //     0, 2, 1, 2, 3, 1, 4, 6, 5, 6, 7, 5, 8, 10, 9, 10, 11, 9, 12, 14, 13, 14,
-  //     15, 13, 16, 18, 17, 18, 19, 17, 20, 22, 21, 22, 23, 21,
-  //   ]
-
-  //   const _boxIndeces = [
-  //     0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12,
-  //     14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23,
-  //   ]
-
-  //   const geo = new THREE.BufferGeometry()
-  //   const pos = []
-  //   const uv = []
-  //   const indices = []
-
-  //   // console.log(_boxVertexArray.length)
-
-  //   for (let i = 0; i < NUMBER; i++) {
-  //     for (let j = 0; j < boxVertexArray.length; j++) {
-  //       pos.push(boxVertexArray[j])
-
-  //       uv.push(i / NUMBER)
-  //       uv.push(j / boxVertexArray.length)
-
-  //       if (j % 3 === 0) {
-  //         indices.push(boxIndeces[j] + i * 24) // 24 = boxIndeces.length
-  //       }
-  //     }
-  //   }
-
-  //   geo.setAttribute(
-  //     "position",
-  //     new THREE.BufferAttribute(new Float32Array(pos), 3)
-  //   )
-  //   geo.setAttribute("uv", new THREE.BufferAttribute(new Float32Array(uv), 2))
-  //   geo.setIndex(new THREE.BufferAttribute(new Uint32Array(indices), 1))
-
-  //   this.uni = {
-  //     texturePosition: { value: null },
-  //     textureVelocity: { value: null },
-  //     uvDiff: { value: 1 / WIDTH },
-  //   }
-
-  //   const mat = new THREE.ShaderMaterial({
-  //     uniforms: this.uni,
-  //     vertexShader: vs,
-  //     fragmentShader: fs,
-  //     // wireframe: true,
-  //   })
-
-  //   this.obj = new THREE.Mesh(geo, mat)
-  //   this.obj.matrixAutoUpdate = false
-  //   this.obj.updateMatrix()
-
-  //   // this.obj.castShadow = true
-
-  //   CommonWork.scene?.add(this.obj)
-  // }
-
   createTrails() {
     const box = new THREE.BoxGeometry(20, 20, 20)
     const geo = new THREE.InstancedBufferGeometry()
@@ -157,8 +79,6 @@ export default class App extends TemplateArtwork {
         instancedUv.push(i / WIDTH, j / WIDTH)
       }
     }
-
-    console.log(instancedUv)
 
     geo.setAttribute(
       "offset",
@@ -180,14 +100,11 @@ export default class App extends TemplateArtwork {
       vertexShader: vs,
       fragmentShader: fs,
       side: THREE.DoubleSide,
-      // wireframe: true,
     })
 
     this.obj = new THREE.Mesh(geo, mat)
     this.obj.matrixAutoUpdate = false
     this.obj.updateMatrix()
-
-    // this.obj.castShadow = true
 
     CommonWork.scene?.add(this.obj)
   }
